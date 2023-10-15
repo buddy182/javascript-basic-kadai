ad7842ee857a23affa2492f0a40f91ca7e703746
buddy182 <144408739+buddy182@users.noreply.github.com>
2023-10-15 14:53:01 +0900

    kadai_005 を追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+// 定数の宣言・値の代入
+let weight;
+
+weight = 68;
+
+// 定数の宣言・値の代入
+let height;
+
+height = 1.7;
+
+// 定数の宣言・値の代入
+let bmi;
+
+bmi = weight / (height * height);
+
+// コンソールへの出力
+console.log(bmi);
