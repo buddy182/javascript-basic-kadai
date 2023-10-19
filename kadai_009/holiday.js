4722725c441f4bcba21b650f0af60493194d0837
buddy182 <144408739+buddy182@users.noreply.github.com>
2023-10-19 21:54:05 +0900

    kadai_009を追加

---
+++ kadai_009/holiday.js
@@ -0,0 +1,18 @@
+//　出力する　祝日
+let holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
+console.log(holidays);
+
+
+// for構文
+for (let i = 0; i <= 15; i += 1) {
+  console.log(holidays[i]);
+}
+
+
+//　While構文
+let c = 0;
+
+while (c <= 15) {
+  console.log(holidays[c]);
+  c = c+1;
+} 
